5c17cdb4d773afc1d02491d9b33af7bbeb73a24e
YukinaMinato <1219377438@qq.com>
2026-09-04 13:46:42 +0800

    Allow building without Qt TextToSpeech

--- texttospeechmanager.cpp
+++ texttospeechmanager.cpp
@@ -2,23 +2,35 @@
 #include "TextToSpeechManager.h"
 
 TextToSpeechManager::TextToSpeechManager(QObject *parent)
+#if REBULID_HAS_TEXT_TO_SPEECH
     : QObject(parent), textToSpeech(new QTextToSpeech(this))
+#else
+    : QObject(parent)
+#endif
 {
+#if REBULID_HAS_TEXT_TO_SPEECH
     // 可以选择语音
     QList<QVoice> voices = textToSpeech->availableVoices();
     if (!voices.isEmpty()) {
         textToSpeech->setVoice(voices.first());
     }
+#endif
 }
 
 TextToSpeechManager::~TextToSpeechManager()
 {
+#if REBULID_HAS_TEXT_TO_SPEECH
     delete textToSpeech;
+#endif
 }
 
 void TextToSpeechManager::speak(const QString &text)
 {
+#if REBULID_HAS_TEXT_TO_SPEECH
     if (textToSpeech) {
         textToSpeech->say(text);
     }
+#else
+    Q_UNUSED(text)
+#endif
 }
--- texttospeechmanager.h
+++ texttospeechmanager.h
@@ -3,8 +3,10 @@
 #define TEXTTOSPEECHMANAGER_H
 
 #include <QObject>
+#if REBULID_HAS_TEXT_TO_SPEECH
 #include <QTextToSpeech>
 #include <QVoice>
+#endif
 
 class TextToSpeechManager : public QObject
 {
@@ -16,7 +18,9 @@ public:
     Q_INVOKABLE void speak(const QString &text);
 
 private:
+#if REBULID_HAS_TEXT_TO_SPEECH
     QTextToSpeech *textToSpeech;
+#endif
 };
 
 #endif // TEXTTOSPEECHMANAGER_H
